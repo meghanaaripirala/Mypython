e01549d94baa8513a82c9edb4b68812e7b0f8a4c
meghanaaripirala <saigaythrimeghana312@gmail.com>
2025-06-03 14:51:09 +0530

    leap year

---
+++ Python.py/Leap.py
@@ -0,0 +1,12 @@
+y=int(input("enter the year ="))
+if y%100==0:
+    if y%400==0:
+      print("it is a leap year")
+    else :
+       print("it is not leap year")
+else :
+   if y%4==0:
+      print("it is leap year")
+   else :
+      print("it is not leap year")
+      
