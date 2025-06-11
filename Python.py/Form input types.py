2f3a3c5f1bdb0f76a1d55ad916ba98329600e058
meghanaaripirala <saigaythrimeghana312@gmail.com>
2025-06-11 20:32:17 +0530

    input types

---
+++ Python.py/Form input types.py
@@ -0,0 +1,25 @@
+import re
+name= input("enter your name")
+if re.fullmatch(r"[A-Za-z\.]+",name):
+    print("valid name using first name")
+else :
+    print("it is not valid name")
+
+email = input("Enter your email: ")
+if re.fullmatch('[\w.]*@(gmail|yahoo|outlook){1}\.com{1}',email):
+    print("Valid email")
+else:
+    print("Invalid email")
+
+phone = input("Enter your phone number: ")
+if re.fullmatch(r"[6-9][0-9]{9}", phone):
+    print("Valid phone number")
+else:
+    print("Invalid phone number")
+password = input("Enter password: ")
+if re.fullmatch(r"[A-Za-z0-9]{6,}", password):
+    print(" Valid password (letters and numbers only)")
+else:
+    print("Use only letters and numbers (min 6 characters)")
+
+    
---
+++ Python.py/pattersfor.py
@@ -0,0 +1,8 @@
+for i in range(1):
+  for j in range(6):
+      if i==0 or j==0 or i==6-1 or j == 6-1 :
+        print('*',end=' ')
+      else:
+         print(' ',end=' ') 
+print(' ')
+
